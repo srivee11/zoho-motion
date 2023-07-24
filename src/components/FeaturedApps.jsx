e245da93e742b90c5f1a94c3d317853cf331079c
srivee11 <srivee11@gmail.com>
2023-07-24 18:34:18 +0530

    added blue top ellipse gradient blobs f_apps

--- src/components/FeaturedApps.jsx
+++ src/components/FeaturedApps.jsx
@@ -32,7 +32,8 @@ export default function FeaturedApps () {
 
   const card_icons = {
     offscreen: {
-      scale: 0,rotate: -20,
+      scale: 0,
+      rotate: -20
     },
     onscreen: {
       scale: 1,
@@ -42,7 +43,6 @@ export default function FeaturedApps () {
         bounce: 0.3,
         duration: 1,
         ease: 'easeIn'
-
       }
     }
   }
@@ -104,9 +104,14 @@ export default function FeaturedApps () {
 
   return (
     <div
-      className=' w-full h-fit flex flex-col justify-start items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-light via-blue-50 to-white'
+      className=' overflow-hidden relative w-full h-fit flex flex-col justify-start items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]'
       name='products'
     >
+      <div className=' mx-auto absolute top-[-35vmin] left-0 right-0 h-full max-h-[60vmin] w-full max-w-[100vmin] bg-indigo-500 rounded-full mix-blend-lighten opacity-80 filter blur-xl'></div>
+      <div className=' mx-auto absolute top-[-40vmin] left-0 right-0 h-full max-h-[80vmin] w-full max-w-[130vmin] bg-blue-500 rounded-full mix-blend-soft-light opacity-60 filter blur-xl'></div>
+      <div className=' mx-auto absolute top-[-50vmin] left-0 right-0 h-full max-h-[100vmin] w-full max-w-[150vmin] bg-cyan-300 rounded-full mix-blend-soft-light opacity-60 filter blur-xl'></div>
+      <div className=' mx-auto absolute top-[-100vmin] left-0 right-0 aspect-square w-full max-w-[200vmin] bg-gradient-to-b from-cyan-300 to bg-white  mix-blend-soft-light opacity-50 filter blur-xl'></div>
+
       <div className='relative  max-w-screen-lg w-full h-full flex flex-col items-center text-center py-12 text-white lg:py-32'>
         <div>
           <h3 className=' capitalize text-2xl font-Mont font-medium tracking-wide text-gray-900  p-2 sm:text-3xl '>
@@ -128,7 +133,14 @@ export default function FeaturedApps () {
                 'product-card relative group  min-h-[300px] w-full max-w-[300px] bg-white border-2 border-gray-200 px-4 py-2 rounded-xl flex flex-col items-center overflow-hidden'
               }
             >
-             <motion.span variants={card_icons}> <img src={src} alt='product_icons' className='w-[96px] mx-auto' /></motion.span> 
+              <motion.span variants={card_icons}>
+                {' '}
+                <img
+                  src={src}
+                  alt='product_icons'
+                  className='w-[96px] mx-auto'
+                />
+              </motion.span>
               <p className=' capitalize text-2xl font-Mont font-bold mt-4 tracking-wide inline-block text-gray-900'>
                 {title}
               </p>
@@ -154,14 +166,9 @@ export default function FeaturedApps () {
             <FaChevronRight className=' ml-1' size={16} />
           </span>
         </button>
-            
-      {/* <hr className=' absolute bottom-0 mt-1 w-[50%] h-[1px] border border-gray-200 rounded-full  '></hr> */}
-          
 
+        {/* <hr className=' absolute bottom-0 mt-1 w-[50%] h-[1px] border border-gray-200 rounded-full  '></hr> */}
       </div>
-
-
-
     </div>
   )
 }
